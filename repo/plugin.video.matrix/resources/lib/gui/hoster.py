e0b96f14ca89aa80c972e102e4322a188f2fd599
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2023-12-09 14:10:38 +0200

    Update hoster.py

--- repo/plugin.video.matrix/resources/lib/gui/hoster.py
+++ repo/plugin.video.matrix/resources/lib/gui/hoster.py
@@ -413,7 +413,7 @@ class cHosterGui:
             or ('ved2om' in sHostName) or ('viidboom' in sHostName) or ('vid2bom' in sHostName) or ('vig1bm' in sHostName)\
             or ('v3db1oom' in sHostName) or ('vdp1em' in sHostName) or ('ved1om' in sHostName) or ('vvid1om' in sHostName)\
             or ('vigom' in sHostName) or ('ve1dp3m' in sHostName) or ('vuidbeaam' in sHostName) or ('v2ddb3m' in sHostName)\
-            or ('2vbiim' in sHostName) or ('vdb123m' in sHostName) or ('vd123bm' in sHostName):
+            or ('2vbiim' in sHostName) or ('vdb123m' in sHostName) or ('vd123bm' in sHostName) or ('v3dbeam' in sHostName):
             return self.getHoster('vidbom')
 				
         if ('mail.ru' in sHostName):
@@ -540,7 +540,7 @@ class cHosterGui:
             or ('gov1ad' in sHostName) or ('go2ved' in sHostName) or ('go1ved' in sHostName) or ('go-veid' in sHostName)\
             or ('g1ov3d' in sHostName)  or ('g1v3d' in sHostName) or ('g2vfd' in sHostName) or ('goo1vd' in sHostName)\
             or ('g2ev4d' in sHostName) or ('ge1verd' in sHostName) or ('g1oov1d' in sHostName) or ('ga1ov3d' in sHostName)\
-            or ('1gafv3d' in sHostName)or ('go12d' in sHostName):
+            or ('1gafv3d' in sHostName)or ('go12d' in sHostName) or ('go1v2d' in sHostName):
             return self.getHoster('govidme')
             
         if ('nowvid' in sHostName) or ('vegaasvid' in sHostName)or('govid' in sHostName) or ('drkvid' in sHosterUrl) \
