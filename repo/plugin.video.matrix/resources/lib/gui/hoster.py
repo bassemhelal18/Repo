38bd90a37f93a079064a6f07dab74078101c1bac
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2024-01-08 14:00:53 +0200

    Update hoster.py

--- repo/plugin.video.matrix/resources/lib/gui/hoster.py
+++ repo/plugin.video.matrix/resources/lib/gui/hoster.py
@@ -329,7 +329,8 @@ class cHosterGui:
         if ('liiivideo' in sHostName):
             return self.getHoster('qfilm')
         
-        if ('vidhide' in sHostName) or ('tuktukcinema29.buzz'in sHostName):
+        vidhide = next((x for x in ['vidhide', 'tuktukcinema29.buzz', 'anime7u'] if x in sHostName), None)
+        if vidhide:    
             return self.getHoster('vidhide')
 
         if ('rumble' in sHostName):
