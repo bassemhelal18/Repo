587acab7e62c0c4a53874f233fb3edaa30f138e3
bassemhelal18 <77381611+bassemhelal18@users.noreply.github.com>
2024-04-02 23:02:52 +0200

    Update faselhd.py

--- repo/plugin.video.matrix/resources/sites/faselhd.py
+++ repo/plugin.video.matrix/resources/sites/faselhd.py
@@ -576,7 +576,7 @@ def prase_function(data):
      t_script = re.findall('<script.*?;.*?\'(.*?);', data, re.S)
      t_int = re.findall('/g.....(.*?)\)', data, re.S)
      if t_script and t_int:
-         script = t_script[0].replace("'",'')
+         script = t_script[2].replace("'",'')
          script = script.replace("+",'')
          script = script.replace("\n",'')
          sc = script.split('.')
@@ -585,7 +585,7 @@ def prase_function(data):
              c_elm = base64.b64decode(elm+'==').decode()
              t_ch = re.findall('\d+', c_elm, re.S)
              if t_ch:
-                nb = int(t_ch[0])+int(t_int[0])
+                nb = int(t_ch[0])+int(t_int[1])
                 page = page + chr(nb)
     return page
 				
